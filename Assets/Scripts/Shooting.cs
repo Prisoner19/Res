f2ac0c17ab2cd869cfff330ada0c7303da00f8a5
Germán Cruz <clz.german@gmail.com>
2016-03-15 10:52:55 -0500

    Project Upload

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour 
+{
+	public SpriteRenderer sprRnd;
+	public Sprite spr_free;
+	public Sprite spr_busy;
+
+	private bool can_shoot;
+
+	// Use this for initialization
+	void Start () 
+	{
+		can_shoot = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void Shoot()
+	{
+		if(can_shoot)
+		{
+			StartCoroutine(Fire_Weapon());
+		}
+	}
+
+	private IEnumerator Fire_Weapon()
+	{
+		can_shoot = false;
+		sprRnd.sprite = spr_busy;
+		yield return new WaitForSeconds(2);
+		can_shoot = true;
+		sprRnd.sprite = spr_free;
+	}
+}
