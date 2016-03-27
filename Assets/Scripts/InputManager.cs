08f375cd6371a0a9fb0082b573eec6ce66fc5578
Germán Cruz <clz.german@gmail.com>
2016-03-27 02:24:40 -0500

    Testing git

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,83 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class InputManager : MonoBehaviour 
+public class InputManager : MonoBehaviour
 {
-	// Use this for references
-	void Awake()
-	{
+    // Use this for references
+    void Awake()
+    {
 
-	}
+    }
 
-	// Use this for initialization
-	void Start () 
-	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		Check_Mouse_Input();
-		Check_Key_Input();
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	private void Check_Mouse_Input()
-	{
-		if(InputTracker.Has_Clicked(Mouse_Button.Left))
-		{
-			if(InputTracker.Get_Object_Under_Mouse("Soldier"))
-			{
-				GameObject go_soldier = InputTracker.Get_Object_Under_Mouse("Soldier");
-				SoldierManager.Instance.Select_Soldier(go_soldier.GetComponent<Soldier>().ID);
-			}
+    }
 
-			GameObject go_enemy_clicked = InputTracker.Get_Object_Under_Mouse("Enemy");
+    // Update is called once per frame
+    void Update()
+    {
+        Check_Mouse_Input();
+        Check_Key_Input();
+    }
 
-			if(go_enemy_clicked != null)
-			{
-				Soldier obj_random_idle_soldier = SoldierManager.Instance.Get_Random_Idle_Soldier();
+    private void Check_Mouse_Input()
+    {
+        Check_Left_Click();
+        Check_Right_Click();
+    }
 
-				if(obj_random_idle_soldier != null)
-				{
-					SoldierManager.Instance.Select_Soldier(obj_random_idle_soldier.ID);
-					bool success = obj_random_idle_soldier.Get_Attacking().Attack(go_enemy_clicked.transform.position);
+    private void Check_Right_Click()
+    {
+       if (InputTracker.Has_Clicked(Mouse_Button.Right))
+        {
+            if (SoldierManager.Instance.Get_Seletected_Soldier() != null)
+            {
+                SoldierManager.Instance.Get_Seletected_Soldier().Get_Attacking().Attack(Vector3.zero);
+            }
+        }
+    }
 
-					if(success)
-					{
-						go_enemy_clicked.GetComponent<Enemy.Info>().Get_Killed();
-					}
-				}
+    private void Check_Left_Click()
+    {
+        if (InputTracker.Has_Clicked(Mouse_Button.Left))
+        {
+            if (InputTracker.Get_Object_Under_Mouse("Soldier"))
+            {
+                GameObject go_soldier = InputTracker.Get_Object_Under_Mouse("Soldier");
+                SoldierManager.Instance.Select_Soldier(go_soldier.GetComponent<Soldier>().ID);
+            }
 
-			}
-		}
+            GameObject go_enemy_clicked = InputTracker.Get_Object_Under_Mouse("Enemy");
 
-		if(InputTracker.Has_Clicked(Mouse_Button.Right))
-		{
-			if(SoldierManager.Instance.Get_Seletected_Soldier() != null)
-			{
-				SoldierManager.Instance.Get_Seletected_Soldier().Get_Attacking().Attack(Vector3.zero);
-			}
-		}
-	}
+            if (go_enemy_clicked != null)
+            {
+                Soldier obj_random_idle_soldier = SoldierManager.Instance.Get_Random_Idle_Soldier();
 
-	private void Check_Key_Input()
-	{
-		if(InputTracker.Has_Pressed_Key(KeyCode.Alpha1))
-		{
-			SoldierManager.Instance.Select_Soldier(1);
-		}
-		else if(InputTracker.Has_Pressed_Key(KeyCode.Alpha2))
-		{
-			SoldierManager.Instance.Select_Soldier(2);
-		}
-		else if(InputTracker.Has_Pressed_Key(KeyCode.Alpha3))
-		{
-			SoldierManager.Instance.Select_Soldier(3);
-		}
-		else if(InputTracker.Has_Pressed_Key(KeyCode.Alpha4))
-		{
-			SoldierManager.Instance.Select_Soldier(4);
-		}
-	}
+                if (obj_random_idle_soldier != null)
+                {
+                    SoldierManager.Instance.Select_Soldier(obj_random_idle_soldier.ID);
+                    bool success = obj_random_idle_soldier.Get_Attacking().Attack(go_enemy_clicked.transform.position);
+
+                    if (success)
+                    {
+                        go_enemy_clicked.GetComponent<Enemy.Info>().Get_Killed();
+                    }
+                }
+
+            }
+        }
+    }
+
+    private void Check_Key_Input()
+    {
+        if (InputTracker.Has_Pressed_Key(KeyCode.Alpha1))
+        {
+            SoldierManager.Instance.Select_Soldier(1);
+        }
+        else if (InputTracker.Has_Pressed_Key(KeyCode.Alpha2))
+        {
+            SoldierManager.Instance.Select_Soldier(2);
+        }
+        else if (InputTracker.Has_Pressed_Key(KeyCode.Alpha3))
+        {
+            SoldierManager.Instance.Select_Soldier(3);
+        }
+        else if (InputTracker.Has_Pressed_Key(KeyCode.Alpha4))
+        {
+            SoldierManager.Instance.Select_Soldier(4);
+        }
+    }
 }
--- Assets/Scripts/Soldier/Attacking.cs
+++ Assets/Scripts/Soldier/Attacking.cs
@@ -1,122 +1,125 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Attacking : MonoBehaviour 
+namespace Soldier
 {
-	private Soldier obj_soldier;
+    public class Attacking : MonoBehaviour 
+    {
+        private Soldier obj_soldier;
 
-	private int current_ammo;
-	private bool can_attack;
+        private int current_ammo;
+        private bool can_attack;
 
-	void Awake()
-	{
-		obj_soldier = gameObject.GetComponent<Soldier>();
-	}
+        void Awake()
+        {
+            obj_soldier = gameObject.GetComponent<Soldier>();
+        }
 
-	void Start () 
-	{
-		can_attack = true;
-		current_ammo = 6;
-	}
-	
-	void Update () 
-	{
-	
-	}
+        void Start () 
+        {
+            can_attack = true;
+            current_ammo = 6;
+        }
+        
+        void Update () 
+        {
+        
+        }
 
-	public bool Attack(Vector3 enemy_position)
-	{
-		float distance = Vector3.Distance(gameObject.transform.position, enemy_position);
-		return Try_To_Shoot();
-//		if(distance < 150)
-//		{
-//			Start_To_Attack_With_Melee();
-//			return true;
-//		}
-//		else
-//		{
-//			return Try_To_Shoot();
-//		}
-	}
+        public bool Attack(Vector3 enemy_position)
+        {
+            float distance = Vector3.Distance(gameObject.transform.position, enemy_position);
+            return Try_To_Shoot();
+    //		if(distance < 150)
+    //		{
+    //			Start_To_Attack_With_Melee();
+    //			return true;
+    //		}
+    //		else
+    //		{
+    //			return Try_To_Shoot();
+    //		}
+        }
 
-	private void Start_To_Attack_With_Melee()
-	{
-		can_attack = false;
-		obj_soldier.Get_Rendering().Change_To_Attack_With_Knife();
-	}
+        private void Start_To_Attack_With_Melee()
+        {
+            can_attack = false;
+            obj_soldier.Get_Rendering().Change_To_Attack_With_Knife();
+        }
 
-	private void Attack_With_Melee()
-	{
-		obj_soldier.Get_Sound_Player().Play_Knife_Sound();
-	}
+        private void Attack_With_Melee()
+        {
+            obj_soldier.Get_Sound_Player().Play_Knife_Sound();
+        }
 
-	private void Finish_To_Attack_With_Melee()
-	{
-		can_attack = true;
-	}
+        private void Finish_To_Attack_With_Melee()
+        {
+            can_attack = true;
+        }
 
-	private bool Try_To_Shoot()
-	{
-		bool success = can_attack;
+        private bool Try_To_Shoot()
+        {
+            bool success = can_attack;
 
-		if(can_attack)
-		{
-			Start_To_Fire_Weapon();
-		}
+            if(can_attack)
+            {
+                Start_To_Fire_Weapon();
+            }
 
-		return success;
-	}
-		
-	private void Start_To_Fire_Weapon()
-	{
-		can_attack = false;
-		Fire_Weapon();
-		obj_soldier.Set_State(SoldierState.Shooting);
-		obj_soldier.Get_Rendering().Change_To_Shooting();
-		obj_soldier.Get_Sound_Player().Play_Shot_Sound();
-	}
+            return success;
+        }
+            
+        private void Start_To_Fire_Weapon()
+        {
+            can_attack = false;
+            Fire_Weapon();
+            obj_soldier.Set_State(SoldierState.Shooting);
+            obj_soldier.Get_Rendering().Change_To_Shooting();
+            obj_soldier.Get_Sound_Player().Play_Shot_Sound();
+        }
 
-	private void Fire_Weapon()
-	{
-		Decrease_Ammo();
-	}
+        private void Fire_Weapon()
+        {
+            Decrease_Ammo();
+        }
 
-	private void Finish_Firing_Weapon()
-	{
-		if(Is_Out_Of_Ammo())
-		{
-			Reload_Weapon();
-		}
-		else
-		{
-			obj_soldier.Get_Rendering().Change_To_Idle();
-			can_attack = true;
-			obj_soldier.Set_State(SoldierState.Idle);
-		}
-	}
+        private void Finish_Firing_Weapon()
+        {
+            if(Is_Out_Of_Ammo())
+            {
+                Reload_Weapon();
+            }
+            else
+            {
+                obj_soldier.Get_Rendering().Change_To_Idle();
+                can_attack = true;
+                obj_soldier.Set_State(SoldierState.Idle);
+            }
+        }
 
-	private void Reload_Weapon()
-	{
-		obj_soldier.Get_Rendering().Change_To_Reloading();
-		obj_soldier.Set_State(SoldierState.Reloading);
-		obj_soldier.Get_Sound_Player().Play_Reload_Sound();
-	}
+        private void Reload_Weapon()
+        {
+            obj_soldier.Get_Rendering().Change_To_Reloading();
+            obj_soldier.Set_State(SoldierState.Reloading);
+            obj_soldier.Get_Sound_Player().Play_Reload_Sound();
+        }
 
-	private void Finish_Reloading_Weapon()
-	{
-		current_ammo = 6;
-		obj_soldier.Get_Rendering().Change_To_Idle();
-		obj_soldier.Set_State(SoldierState.Idle);
-		can_attack = true;
-	}
+        private void Finish_Reloading_Weapon()
+        {
+            current_ammo = 6;
+            obj_soldier.Get_Rendering().Change_To_Idle();
+            obj_soldier.Set_State(SoldierState.Idle);
+            can_attack = true;
+        }
 
-	private void Decrease_Ammo()
-	{
-		current_ammo = (current_ammo > 0) ? current_ammo - 1 : 0;
-	}
+        private void Decrease_Ammo()
+        {
+            current_ammo = (current_ammo > 0) ? current_ammo - 1 : 0;
+        }
 
-	private bool Is_Out_Of_Ammo()
-	{
-		return (current_ammo == 0);
-	}
+        private bool Is_Out_Of_Ammo()
+        {
+            return (current_ammo == 0);
+        }
+    }
 }
